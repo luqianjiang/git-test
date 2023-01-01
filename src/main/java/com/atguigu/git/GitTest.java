8a909aa84e7193475fdd0c3161ed4bdf56bfcd16
luqianjiang <18373351351@163.com>
2023-01-01 21:21:52 +0800

    pust again

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("master git4");
         System.out.println("hello test!");
         System.out.println("push test!");
+        System.out.println("push again!");
     }
 }

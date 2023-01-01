dcdbf3c33c83d133384fd2e65f2fd187ccb08ffc
luqianjiang <83942353+luqianjiang@users.noreply.github.com>
2023-01-01 21:24:29 +0800

    pull test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello test!");
         System.out.println("push test!");
         System.out.println("push again!");
+        System.out.println("pull test!");
     }
 }
